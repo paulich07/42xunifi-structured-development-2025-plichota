a8dbc8870f329ce1652d59ed7ec4697d9f0ad068
Paulina Lichota <plichota@c2r5p6.42firenze.it>
2025-06-10 16:02:09 +0200

    wip

--- sd00/ex00/average.c
+++ sd00/ex00/average.c
@@ -2,28 +2,30 @@
 
 int     is_between_0_and_100(int n)
 {
-    return (n >= 0 && n <= 100);
+	return (n >= 0 && n <= 100);
 }
 
 float   average(const int *arr, int size)
 {
-    int i;
-    int sum;
-    int n;
+	int i;
+	int sum;
+	int n;
 
-    i = 0;
-    sum = 0;
-    n = 0;
-    while (i < size)
-    {
-        if (is_between_0_and_100(arr[i]))
-        {
-            sum += arr[i];
-            n++;
-        }
-        i++;
-    }
-    return (((float) sum) / n);
+	i = 0;
+	sum = 0;
+	n = 0;
+	if (size <= 0 || !arr)
+		return (0);
+	while (i < size)
+	{
+		if (is_between_0_and_100(arr[i]))
+		{
+			sum += arr[i];
+			n++;
+		}
+		i++;
+	}
+	return (((float) sum) / n);
 }
 
 // #include <stdio.h>
@@ -31,5 +33,5 @@ float   average(const int *arr, int size)
 // {
 //     int arr[] = {1, 2, -1, 10};
 //     int size = sizeof(arr) / sizeof(arr[0]);
-//     printf("final sum = %f\n", average(arr, size));
+//     printf("final sum = %f\n", average(arr, 0));
 // }
