9a1926bc909d02ef4967513b6cbf18f0fad0e755
Paulina Lichota <plichota@c2r5p6.42firenze.it>
2025-06-10 19:11:16 +0200

    ex03

---
+++ sd00/ex03/critical_windows.h
@@ -0,0 +1,11 @@
+#ifndef CRITICAL_WINDOWS_H
+# define CRITICAL_WINDOWS_H
+
+# ifndef RANGE
+#  define RANGE 5
+# endif
+
+int count_critical_windows(const int *readings, int size);
+static int is_critical(const int *readings, int start_index);
+
+#endif
