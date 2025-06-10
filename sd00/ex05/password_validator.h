57a42c35ffb21ac7f9430ece044c71d79d6a9b80
Paulina Lichota <plichota@c2r5p6.42firenze.it>
2025-06-10 23:08:25 +0200

    ex05

---
+++ sd00/ex05/password_validator.h
@@ -0,0 +1,17 @@
+#ifndef PASSWORD_VALIDATOR_H
+# define PASSWORD_VALIDATOR_H
+
+# include <stdbool.h>
+
+typedef enum status {
+    VALID,
+    INVALID
+} PwStatus;
+
+int is_special_char(char c);
+int is_digit(char c);
+int is_lowercase(char c);
+int is_uppercase(char c);
+PwStatus validate_password(const char *new_pw, const char *curr_pw);
+
+#endif
