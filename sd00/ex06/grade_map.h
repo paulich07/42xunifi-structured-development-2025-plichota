d08faddef83ae3da9de03187f21668974da02e1f
Paulina Lichota <plichota@c2r6p8.42firenze.it>
2025-06-11 15:16:50 +0200

    ex06

---
+++ sd00/ex06/grade_map.h
@@ -0,0 +1,17 @@
+#ifndef GRADE_MAP_H
+# define GRADE_MAP_H
+
+typedef struct grade
+{
+    int min;
+    int max;
+    char *grade;
+} Grade;
+
+typedef const char *GradeReturn;
+
+typedef GradeReturn GradeMapper(int n);
+
+void map_scores(const int *scores, int size, GradeMapper mapper, const char **mapped_grades);
+
+#endif
